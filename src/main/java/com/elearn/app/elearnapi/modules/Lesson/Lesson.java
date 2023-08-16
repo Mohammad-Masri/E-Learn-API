23f62dee7c9bac9d39583344801fe8727d36d233
Muhammad Al Masri <muhammadalmasri@MalishX-2.local>
2023-08-16 15:35:04 +0400

    ADD | lesson module

---
+++ src/main/java/com/elearn/app/elearnapi/modules/Lesson/Lesson.java
@@ -0,0 +1,95 @@
+package com.elearn.app.elearnapi.modules.Lesson;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int order;
+    private String title;
+    private String description;
+    private String URL;
+    private Boolean isFree;
+    private Boolean isPublished;
+
+    public Lesson() {
+    }
+
+    public Lesson(
+            int order,
+            String title,
+            String description,
+            String URL) {
+
+        this.order = order;
+        this.title = title;
+        this.description = description;
+        this.URL = URL;
+        this.isFree = false;
+        this.isPublished = false;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getIsFree() {
+        return isFree;
+    }
+
+    public void setIsFree(Boolean isFree) {
+        this.isFree = isFree;
+    }
+
+    public Boolean getIsPublished() {
+        return isPublished;
+    }
+
+    public void setIsPublished(Boolean isPublished) {
+        this.isPublished = isPublished;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String uRL) {
+        URL = uRL;
+    }
+
+}
