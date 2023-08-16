ae51c43684a4802b616671bd63a86c6d02669921
Muhammad Al Masri <muhammadalmasri@MalishX-2.local>
2023-08-16 09:53:36 +0400

    ADD | is admin filter

--- src/main/java/com/elearn/app/elearnapi/config/filters/IsAdminFilter.java
+++ src/main/java/com/elearn/app/elearnapi/config/filters/IsAdminFilter.java
@@ -2,9 +2,13 @@ package com.elearn.app.elearnapi.config.filters;
 
 import java.io.IOException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.elearn.app.elearnapi.utilities.PrintUtilities;
+import com.elearn.app.elearnapi.config.constants.UserRole;
+import com.elearn.app.elearnapi.errors.ErrorResponse;
+import com.elearn.app.elearnapi.errors.HTTPServerError;
+import com.elearn.app.elearnapi.utilities.JsonUtilities;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -19,8 +23,20 @@ public class IsAdminFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain chain) throws IOException, ServletException {
 
-        String id = (String) request.getAttribute("id");
         String role = (String) request.getAttribute("role");
+
+        if (role == null || !role.equals(UserRole.ADMIN.toString())) {
+            try {
+                throw new HTTPServerError(HttpStatus.UNAUTHORIZED, "this action is not allowed");
+            } catch (HTTPServerError e) {
+                ErrorResponse errorResponse = new ErrorResponse(e.getStatusCode(),
+                        e.getMessage());
+                response.setContentType("application/json");
+                response.setStatus(e.getStatusCode());
+                response.getWriter().write(JsonUtilities.convertToJson(errorResponse));
+                return;
+            }
+        }
 
         chain.doFilter(request, response);
 
