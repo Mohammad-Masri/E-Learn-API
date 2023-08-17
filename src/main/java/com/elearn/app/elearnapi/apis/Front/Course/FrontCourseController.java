6437fd0d427758d10edcd20796f3cb9d4cfc384c
Muhammad Al Masri <muhammadalmasri@MalishX-2.local>
2023-08-17 15:35:32 +0400

    ADD | front course controller

---
+++ src/main/java/com/elearn/app/elearnapi/apis/Front/Course/FrontCourseController.java
@@ -0,0 +1,10 @@
+package com.elearn.app.elearnapi.apis.Front.Course;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/front/courses")
+public class FrontCourseController {
+
+}
