7ce9cbc41f83fa83764f74730121aae8b78223e5
Muhammad Al Masri <muhammadalmasri@MalishX-2.local>
2023-08-17 12:35:50 +0400

    ADD | swagger link

--- src/main/java/com/elearn/app/elearnapi/ElearnApiApplication.java
+++ src/main/java/com/elearn/app/elearnapi/ElearnApiApplication.java
@@ -3,11 +3,15 @@ package com.elearn.app.elearnapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.elearn.app.elearnapi.utilities.PrintUtilities;
+
 @SpringBootApplication
 public class ElearnApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElearnApiApplication.class, args);
+
+		PrintUtilities.println("Swagger UI at : http://localhost:8080/api/swagger-ui/index.html");
 	}
 
 }
